5b2d2311da0b9dc7135f72165b291e3e021f3aeb
Swasthik S <136150261+swasthik-s@users.noreply.github.com>
2025-04-17 21:58:36 +0530

    Update supabase.ts

--- lib/supabase.ts
+++ lib/supabase.ts
@@ -1,9 +1,8 @@
-// lib/supabase.ts
 import { createClient } from '@supabase/supabase-js';
 
 const supabase = createClient(
-  process.env.NEXT_PUBLIC_SUPABASE_URL ?? '',
-  process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY ?? ''
+  process.env.SUPABASE_URL!,
+  process.env.SUPABASE_ANON_KEY!
 );
 
 export { supabase };
